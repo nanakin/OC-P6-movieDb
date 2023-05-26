b40e13de758d8c9d1663bea2358d003abb202afb
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-05-26 12:21:36 +0200

    use fetch/await instead of XHR objects

--- movies.js
+++ movies.js
@@ -7,25 +7,13 @@ const NB_VISIBLE_TITLES = 4;
 const genres = {}; // To store genres data retrieved from the API
 const titles = {}; // To store all movies data once retrieved from the API
 
-// Initialize most of XHR objects used by the website -----------------------------------
-function initXHR () {
-  const requests = {};
-  requests.genres = new XMLHttpRequest(); // To make requests to API_URL/genres
-  requests.genres.onload = getGenres;
-  requests.movieDetails = new XMLHttpRequest(); // To make requests to API_URL/ID_movie
-  requests.movieDetails.onload = fillMovieDetails;
-  requests.bestTitle = new XMLHttpRequest(); // To make request to API_URL/titles
-  requests.bestTitle.onload = function () { getTitles(requests.bestTitle, 'bestTitle', NB_TITLES_PER_CATEGORY); };
-  requests.titlesByGenre = {}; // prepare the map to create an XHR object for each category for asynchronus requests (API_URL/titles?genre=)
-  return requests;
-}
-
 // Create all movies sections and the insert --------------------------------------------
 function getAllTitles () {
-  // Create the best movie insert and the best movies slideshow section
+  // API call for best movies data, then create the best films section and the insert
   titles.bestTitle = {};
-  makeGETRequest(requests.bestTitle, API_URL + 'titles?sort_by=-imdb_score');
-
+  getTitles(API_URL + 'titles?sort_by=-imdb_score', 'bestTitle', NB_TITLES_PER_CATEGORY).then((data) => {
+    createElements(data, 'bestTitle');
+  });
   // Create 3 sections (1 per category) with slideshow
   const selectedCategoriesID = [3, 5, 17]; // crime, history and sport
   const nav = document.getElementsByClassName('dropdown-content')[0];
@@ -36,37 +24,31 @@ function getAllTitles () {
     categoryAnchorLink.href = '?#' + categoryID;
     categoryAnchorLink.innerHTML = genres[categoryID];
     nav.append(categoryAnchorLink);
-    // Create an XHR object for each category for asynchronus requests
+    // API call for data then create category section
     titles[categoryID] = {};
-    requests.titlesByGenre[categoryID] = new XMLHttpRequest();
-    requests.titlesByGenre[categoryID].onload = function () { getTitles(requests.titlesByGenre[categoryID], categoryID, NB_TITLES_PER_CATEGORY); };
     const url = API_URL + 'titles?genre=' + genres[categoryID] + '&sort_by=-imdb_score';
-    // Recursive calls to get the required number of movies (pagination)
-    makeGETRequest(requests.titlesByGenre[categoryID], url);
+    getTitles(url, categoryID, NB_TITLES_PER_CATEGORY).then((data) => {
+      createElements(data, categoryID);
+    });
   }
 }
 
-// Function called by the "genres" XHR object once the request finished
-// Parse data, make further genres requests, then continue with sections creations (and titles requests).
-function getGenres () {
-  if (requests.genres.status === 200) {
-    const response = JSON.parse(requests.genres.responseText);
-    // Iterate over genres JSON objects
-    for (const genreObj of response.results) {
-      // Store retrieved data to "genres" variable
-      genres[parseInt(genreObj.id)] = genreObj.name;
-    }
-    if (response.next != null) {
-      // Follow the pagination
-      makeGETRequest(requests.genres, response.next);
-    } else {
-      // End of pagination (all categories retrieved)
-      // Go now creates movies sections
-      getAllTitles();
-    }
-  } else {
-    console.log('Request problem: ' + requests.genres.status);
-  }
+// Fetch all genre data following pagination
+async function getGenres () {
+  let nextPage = API_URL + 'genres';
+  try {
+    do {
+      const response = await fetch(nextPage);
+      const data = await response.json();
+      nextPage = data.next;
+      for (const genreObj of data.results) {
+        genres[parseInt(genreObj.id)] = genreObj.name;
+      }
+    } while (nextPage != null);
+    return genres;
+  } catch (error) {
+    console.log(error);
+  };
 }
 
 // Function called by an arrow (slideshow) clic
@@ -103,72 +85,78 @@ function moveSlideshow (event) {
   }
 }
 
-// Function called by the "movieDetails" XHR object once the request finished
-function fillMovieDetails () {
-  if (requests.movieDetails.status === 200) {
-    // Parse the data received
-    const movieData = JSON.parse(requests.movieDetails.responseText);
-    // Use it to fill the modal
-    document.getElementById('title').innerText = movieData.title;
-    const movieImage = document.getElementById('cover');
-    movieImage.src = movieData.image_url;
-    movieImage.alt = movieData.title;
-    document.getElementById('genres').innerText = movieData.genres.join(', ');
-    document.getElementById('date').innerText = movieData.date_published;
-    document.getElementById('rated').innerText = movieData.rated;
-    document.getElementById('imdb').innerText = movieData.imdb_score;
-    document.getElementById('directors').innerText = movieData.directors.join(', ');
-    document.getElementById('actors').innerText = movieData.actors.join(', ');
-    document.getElementById('duration').innerText = movieData.duration;
-    document.getElementById('countries').innerText = movieData.countries.join(', ');
-    document.getElementById('income').innerText = movieData.worldwide_gross_income;
-    document.getElementById('description').innerText = movieData.description;
-    // Display the modal
-    modal.style.display = 'block';
-    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
-  } else {
-    console.log('Request problem: ' + requests.movieDetails.status);
-  }
+// Call the API to get the movie details then fill the modal
+function getAndFillDetails (id) {
+  getMovieDetails(id).then((data) => {
+    fillMovieDetails(data);
+  });
 }
 
-// From a movie ID, make the API GET request to get the details (which will call "fillMovieDetails" once finished)
-function getMovieDetails (id) {
+// Call the API to get the movie details then fill the insert
+function getAndFillInsert (id) {
+  getMovieDetails(id).then((data) => {
+    fillBestMovieData(data);
+  });
+}
+
+// Fill the modal
+function fillMovieDetails (movieData) {
+  document.getElementById('title').innerText = movieData.title;
+  const movieImage = document.getElementById('cover');
+  movieImage.src = movieData.image_url;
+  movieImage.alt = movieData.title;
+  document.getElementById('genres').innerText = movieData.genres.join(', ');
+  document.getElementById('date').innerText = movieData.date_published;
+  document.getElementById('rated').innerText = movieData.rated;
+  document.getElementById('imdb').innerText = movieData.imdb_score;
+  document.getElementById('directors').innerText = movieData.directors.join(', ');
+  document.getElementById('actors').innerText = movieData.actors.join(', ');
+  document.getElementById('duration').innerText = movieData.duration;
+  document.getElementById('countries').innerText = movieData.countries.join(', ');
+  document.getElementById('income').innerText = movieData.worldwide_gross_income;
+  document.getElementById('description').innerText = movieData.description;
+  // Display the modal
+  modal.style.display = 'block';
+  document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+}
+
+// From a movie ID, make the API GET request to get the movie details
+async function getMovieDetails (id) {
   const url = API_URL + 'titles/' + id;
-  makeGETRequest(requests.movieDetails, url);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  };
 }
 
-// Function called by the "movieDetails" XHR object once the request finished (called only once, for the best movie insert)
 // Create the best movie insert
-function fillBestMovieData () {
-  if (requests.movieDetails.status === 200) {
-    requests.movieDetails.onload = fillMovieDetails; // next calls will be to fill the modal
-    const movieData = JSON.parse(requests.movieDetails.responseText);
-    const element = movieData;
-    const divBestMovie = document.createElement('div');
-    const divInfo = document.createElement('div');
-    const titleBestMovie = document.createElement('h3');
-    titleBestMovie.innerText = element.title;
-    divInfo.append(titleBestMovie);
-    const imgBestMovie = document.createElement('img');
-    imgBestMovie.className = 'clickable';
-    imgBestMovie.src = element.image_url;
-    imgBestMovie.alt = element.title;
-    imgBestMovie.onclick = function () {
-      getMovieDetails(movieData.id);
-    };
-    const buttonPlay = document.createElement('button');
-    buttonPlay.className = 'btn';
-    buttonPlay.innerText = 'Play';
-    divInfo.append(buttonPlay);
-    const descriptionBestMovie = document.createElement('p');
-    descriptionBestMovie.innerText = element.description;
-    divInfo.append(descriptionBestMovie);
-    divBestMovie.append(divInfo);
-    divBestMovie.append(imgBestMovie);
-    document.getElementById('best_title').append(divBestMovie);
-  } else {
-    console.log('Request problem : ' + requests.movieDetails.status);
-  }
+function fillBestMovieData (movieData) {
+  const element = movieData;
+  const divBestMovie = document.createElement('div');
+  const divInfo = document.createElement('div');
+  const titleBestMovie = document.createElement('h3');
+  titleBestMovie.innerText = element.title;
+  divInfo.append(titleBestMovie);
+  const imgBestMovie = document.createElement('img');
+  imgBestMovie.className = 'clickable';
+  imgBestMovie.src = element.image_url;
+  imgBestMovie.alt = element.title;
+  imgBestMovie.onclick = function () {
+    getAndFillDetails(movieData.id);
+  };
+  const buttonPlay = document.createElement('button');
+  buttonPlay.className = 'btn';
+  buttonPlay.innerText = 'Play';
+  divInfo.append(buttonPlay);
+  const descriptionBestMovie = document.createElement('p');
+  descriptionBestMovie.innerText = element.description;
+  divInfo.append(descriptionBestMovie);
+  divBestMovie.append(divInfo);
+  divBestMovie.append(imgBestMovie);
+  document.getElementById('best_title').append(divBestMovie);
 }
 
 // Create the elements of a slideshow section
@@ -180,8 +168,7 @@ function createElements (elements, key) {
     section.append(h2);
     section.id = 'best_title';
     const idElement = Object.keys(elements)[0];
-    requests.movieDetails.onload = fillBestMovieData;
-    getMovieDetails(idElement);
+    getAndFillInsert(idElement);
     document.querySelector('main').prepend(section);
     section = document.createElement('section');
     h2 = document.createElement('h2');
@@ -220,7 +207,7 @@ function createElements (elements, key) {
     }
     movieDiv.className = 'clickable';
     movieDiv.onclick = function () {
-      getMovieDetails(idElement);
+      getAndFillDetails(idElement);
     };
 
     const movieData = elements[idElement];
@@ -248,39 +235,31 @@ function createElements (elements, key) {
   }
 }
 
-// Function called by the "titlesByGenre" XHR objects once the request finished
-// Parse data, make further requests end finally create the section
-function getTitles (request, key, nbElements) {
+// Fetch all titles data following pagination
+async function getTitles (url, key, nbElements) {
+  let nextPage = url;
   const variable = titles[key];
-  if (request.status === 200) {
-    const response = JSON.parse(request.responseText);
-    // Iterate over genres JSON objects
-    for (const titleObj of response.results) {
-      // Store retrieved data to "title" variable
-      variable[parseInt(titleObj.id)] = titleObj;
-      if (Object.keys(variable).length === nbElements) {
-        break;
+  try {
+    do {
+      const response = await fetch(nextPage);
+      const data = await response.json();
+      nextPage = data.next;
+      for (const titleObj of data.results) {
+        // Store retrieved data to "title" variable
+        variable[parseInt(titleObj.id)] = titleObj;
+        if (Object.keys(variable).length === nbElements) {
+          break;
+        }
       }
-    }
-    if (Object.keys(variable).length < nbElements && response.next != null) {
-      // Follow the pagination
-      makeGETRequest(request, response.next);
-    } else {
-      // Required number of movies reached, create the section
-      createElements(variable, key);
-    }
-  } else {
-    console.log('Request Problem: ' + request.status);
-  }
-}
-
-// all GET requests are asynchronous and send directly
-function makeGETRequest (requestObj, url) {
-  requestObj.open('GET', url, true);
-  requestObj.send();
+    } while (Object.keys(variable).length < nbElements && nextPage != null);
+    return variable;
+  } catch (error) {
+    console.log(error);
+  };
 }
 
 // Main
-const requests = initXHR();
 const modal = createModal();
-makeGETRequest(requests.genres, API_URL + 'genres');
+getGenres().then(() => {
+  getAllTitles();
+});
